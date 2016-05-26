0aae30f5dc2602e298b048b76f3730f822e1bd10
shevchenko <jorgeshevchenk@gmail.com>
2016-05-26 12:50:23 -0500

    act

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -1,12 +1,4 @@
 <?php
-
-use Cronograma\Test;
-Route::get(
-    '/josbel', function () {
-        Test::jorge();
-    }
-);
-
 Route::get(
     '/', function () {
         if (Session::has('accesos')) {
@@ -77,49 +69,21 @@ Route::get(
             }
             $rutaBD = substr($ruta, 6);
             //si tiene accesoo si accede al inicio o a misdatos
-            return View::make($ruta)->with($valores);
-            /*if (in_array($rutaBD, $accesos) or
+            //return View::make($ruta)->with($valores);
+            if (in_array($rutaBD, $accesos) or
                 $rutaBD == 'inicio' or $rutaBD=='mantenimiento.misdatos') {
                 return View::make($ruta)->with($valores);
             } else
-                return Redirect::to('/admin.inicio');*/
+                return Redirect::to('/admin.inicio');
         } else
             return Redirect::to('/');
         }
     )
 );
 
-Route::controller('llamadaatencion', 'Cronograma\LlamadaAtencion\LlamadaatencionController');
-Route::controller('fechanolaborable', 'Cronograma\Fechanolaborable\FechaNolaborableController');
-Route::controller('plantilla', 'Cronograma\PlantillasWord\PlantillaController');
-Route::controller('categoria', 'Cronograma\Categoria\CategoriaController');
-Route::controller('area', 'AreaController');
 Route::controller('cargo', 'CargoController');
-Route::controller('documento', 'DocumentoController');
-Route::controller('flujo', 'FlujoController');
-Route::controller('flujotiporespuesta', 'FlujoTipoRespuestaController');
 Route::controller('language', 'LanguageController');
-Route::controller('lista', 'ListaController');
 Route::controller('menu', 'MenuController');
 Route::controller('opcion', 'OpcionController');
 Route::controller('persona', 'PersonaController');
-Route::controller('reporte', 'ReporteController');
-Route::controller('rol', 'RolController');
-Route::controller('ruta', 'RutaController');
-Route::controller('ruta_detalle', 'RutaDetalleController');
-Route::controller('ruta_flujo', 'RutaFlujoController');
-Route::controller('rf', 'RFController');
-Route::controller('software', 'SoftwareController');
-Route::controller('tabla_relacion', 'TablaRelacionController');
-Route::controller('tiempo', 'TiempoController');
-Route::controller('tiporespuesta', 'TipoRespuestaController');
-Route::controller('tiporespuestadetalle', 'TipoRespuestaDetalleController');
-Route::controller('tiposolicitante', 'TipoSolicitanteController');
-Route::controller('tramite', 'VisualizacionTramiteController');
 Route::controller('usuario', 'UsuarioController');
-Route::controller('verbo', 'VerboController');
-Route::controller('carta', 'CartaController');
-Route::controller('tiporecurso', 'TipoRecursoController');
-Route::controller('tipoactividad', 'TipoActividadController');
-Route::controller('reportef', 'ReporteFinalController');
-Route::controller('fechanolaborable', 'Cronograma\Fechanolaborable\FechanolaborableController');
