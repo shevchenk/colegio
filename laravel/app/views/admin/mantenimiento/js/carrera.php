3b75897d0993f81422a5a3d70db843720552c8a0
Vic Omar <vomar24@gmail.com>
2016-05-30 19:54:36 -0500

    Vistas carreras

---
+++ laravel/app/views/admin/mantenimiento/js/carrera.php
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+$(document).ready(function() {
+
+	Carreras.CargarOpciones(activarTabla);
+
+});
+
+activarTabla=function(){
+	$("#t_carreras").dataTable();
+};
+
+</script>
