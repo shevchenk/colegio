f0760f479fba5bd79a95d54cce35c715b533d566
shevchenk <jorgeshevchenk@gmail.com>
2017-01-14 13:38:09 -0500

    actualizando routes

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -1,8 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *'); 
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE'); 
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
-
 Route::get(
     '/', function () {
         if (Session::has('accesos')) {
